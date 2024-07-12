660fbb14932851b2aa72c3b207090a074e06f5f2
Curt <curtisod.123@gmail.com>
2024-07-11 20:13:19 -0600

    Add unit type check

--- lua/convert/convert_all.lua
+++ lua/convert/convert_all.lua
@@ -1,7 +1,27 @@
 local utils = require("convert.utils")
 local calculator = require("convert.calculator")
+local units = require("convert.units")
 
 local convert_all = function(bufnr, from, to)
+	local from_type = nil
+	local to_type = nil
+	if utils.contains(units.size_units, from) then
+		from_type = 'color'
+	else
+		from_type = 'size'
+	end
+
+	if utils.contains(units.size_units, to) then
+		to_type = 'color'
+	else
+		to_type = 'size'
+	end
+	if from_type ~= to_type then
+		print(string.format("Can't convert %s to %s", from_type, to_type))
+		return
+	end
+
+
 	local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
 	for row = 1, #lines, 1 do
 		local line = lines[row]
