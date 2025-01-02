3af3f1c771112f32205d9fa8bddfdd9f0bddf55d
Curtis <curtisod.123@gmail.com>
2025-01-02 08:52:32 -0700

    feat: support number conversions hex, octal, binary

--- lua/convert/calculator.lua
+++ lua/convert/calculator.lua
@@ -3,7 +3,6 @@ local utils = require("convert.utils")
 
 local M = {}
 
-
 ---@param from string
 ---@param to string
 ---@param val number
@@ -18,12 +17,22 @@ M.convert = function(from, to, val)
 			return tostring(val)
 		end
 
+		if (from == "binary" or "hexadecimal" or "octal") then
+			local cases = {
+				["binary"] = function () return "0b" .. val end,
+				["hexadecimal"] = function () return "0x" .. val end,
+				["octal"] = function () return "0o" .. val end,
+			}
+
+			return tostring((cases[from])())
+		end
+
 		return val .. from
 	end
 
 	local round = 0
 
-	if (to ~= 'px') then
+	if to ~= 'px' then
 		round = 2
 	end
 
@@ -35,6 +44,18 @@ M.convert = function(from, to, val)
 
 	if to == 'hex' then
 		return "#" .. result
+	end
+
+	if to == 'binary' then
+		return "0b".. result
+	end
+
+	if to == 'hexadecimal' then
+		return "0x".. result
+	end
+
+	if to == 'octal' then
+		return "0o".. result
 	end
 
 	return tostring(utils.round(result, round)) .. to
