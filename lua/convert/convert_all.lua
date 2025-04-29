7ea93eb60b767553115bb32733e9f12cac70f77e
Curtis <cjododev@gmail.com>
2025-04-29 09:44:44 -0600

    fix #16

--- lua/convert/convert_all.lua
+++ lua/convert/convert_all.lua
@@ -21,19 +21,25 @@ local convert_all = function(bufnr, from, to)
 		return
 	end
 
-
 	local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
 	for row = 1, #lines, 1 do
 		local line = lines[row]
 
-		local found_unit = utils.find_unit_in_line(line, row)
+		local all_units = utils.find_all_units_in_line(line, row)
+		if all_units ~= nil and #all_units > 0 and all_units[1].unit == from then
+			for i = #all_units, 1, -1 do
+				local found = all_units[i]
+				local converted = calculator.convert(from, to, found.val)
 
-		if found_unit ~= nil and found_unit.unit == from then
-			local converted = calculator.convert(from, to, found_unit.val)
-
-			vim.api.nvim_buf_set_text(bufnr, found_unit.pos.row - 1, found_unit.pos.start_col - 1, found_unit.pos.row - 1,
-				found_unit.pos.end_col,
-				{ converted })
+				vim.api.nvim_buf_set_text(
+					bufnr,
+					found.pos.row - 1,
+					found.pos.start_col - 1,
+					found.pos.row - 1,
+					found.pos.end_col,
+					{ converted }
+				)
+			end
 		end
 	end
 end
